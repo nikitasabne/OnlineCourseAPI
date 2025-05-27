c7680c52d044e6aeedf6241bd2683c49dc922a56
Nikita <nikitej01@gmail.com>
2025-05-27 15:42:00 +0530

    bug fixes

---
+++ OnlineCourse.Data/VideoRequestRepository.cs
@@ -0,0 +1,54 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using OnlineCourse.Core.Entities;
+
+//namespace OnlineCourse.Data
+//{
+//    public class VideoRequestRepository : IVideoRequestRepository
+//    {
+//        private readonly OnlineCourseDbContext onlineCourseDbContext;
+
+//        public VideoRequestRepository(OnlineCourseDbContext onlineCourseDbContext)
+//        {
+//            this.onlineCourseDbContext = onlineCourseDbContext;
+//        }
+//        public async Task<VideoRequest> AddAsync(VideoRequest videoRequest)
+//        {
+//            onlineCourseDbContext.VideoRequests.Add(videoRequest);
+//            await onlineCourseDbContext.SaveChangesAsync();
+//            return videoRequest;
+//        }
+
+//        public async Task DeleteAsync(int id)
+//        {
+//            var videoRequest = await GetVideoByIdAsync(id);
+//            if (videoRequest != null)
+//            {
+//                onlineCourseDbContext.VideoRequests.Remove(videoRequest);
+//                await onlineCourseDbContext.SaveChangesAsync();
+//            }
+//        }
+
+//        public async Task<IEnumerable<VideoRequest>> GetAllAsync()
+//        {
+//            return await onlineCourseDbContext.VideoRequests.Include(x => x.User).ToListAsync();
+//        }
+
+//        public Task<VideoRequest?> GetVideoByIdAsync(int id)
+//        {
+//            return onlineCourseDbContext.VideoRequests.Include(x => x.User)
+//                .FirstOrDefaultAsync(y => y.VideoRequestId == id);
+//        }
+
+//        public async Task<IEnumerable<VideoRequest>> GetVideoByUserIdAsync(int userId)
+//        {
+//            return await onlineCourseDbContext.VideoRequests.Include(x => x.User).Where(y => y.UserId == userId).ToListAsync();
+//        }
+
+//        public async Task<VideoRequest> UpdateAsync(VideoRequest videoRequest)
+//        {
+//            onlineCourseDbContext.VideoRequests.Update(videoRequest);
+//            await onlineCourseDbContext.SaveChangesAsync();
+//            return videoRequest;
+//        }
+//    }
+//}
---
+++ OnlineCourse.Service/IVideoRequestService.cs
@@ -0,0 +1,20 @@
+﻿//using OnlineCourse.Core.DTO;
+//using OnlineCourse.Core.Entities;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OnlineCourse.Service
+//{
+//    public interface IVideoRequestService
+//    {
+//        Task<List<VideoRequestDto>> GetAllAsync();
+//        Task<VideoRequestDto?> GetVideoByIdAsync(int id);
+//        Task<IEnumerable<VideoRequestDto>> GetVideoByUserIdAsync(int userId);
+//        Task<VideoRequestDto> AddAsync(VideoRequestDto videoRequestDto);
+//        Task<VideoRequestDto> UpdateAsync(int id, VideoRequestDto videoRequestDto);
+//        Task DeleteAsync(int id);
+//    }
+//}
